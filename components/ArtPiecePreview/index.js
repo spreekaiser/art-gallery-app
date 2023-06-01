1bfdda9a2dee13edb2f71ee43ab4be1e245538d1
kasulk <danielkaser@mailbox.org>
2023-06-01 06:52:05 +0200

    finish refactor

--- components/ArtPiecePreview/index.js
+++ components/ArtPiecePreview/index.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
 export default function ArtPiecePreview({ image, title, artist }) {
-  console.log(image);
+  // console.log(image);
 
   return (
     <li>
       <h3>{title}</h3>
-      <Image src={image} alt={title} width={360} height={240} />
+        <Image src={image} alt={title} width={360} height={240} />
       <h4>@{artist}</h4>
     </li>
   );
--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,16 @@ import ArtPiecePreview from "../components/ArtPiecePreview";
 export default function HomePage() {
   // const { data } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
   const { data } = useSWR("https://example-apis.vercel.app/api/art");
+
   if (!data) {
     return;
   }
 
-  console.log(data[0].artist);
+  // console.log(data[0].artist);
 
   return (
     <>
       <ArtPieces pieces={data} />
-      <ul>
-        {data.map((piece) => {
-          return (
-            <ArtPiecePreview
-              key={piece.id}
-              image={piece.imageSource}
-              title={piece.name}
-              artist={piece.artist}
-            />
-          );
-        })}
-      </ul>
     </>
   );
 }
