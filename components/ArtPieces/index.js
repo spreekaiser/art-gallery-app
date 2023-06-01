32c15b777c775d9e28d64adda4b942bc3cc0a2da
spreekaiser <thoralf.barth@gmail.com>
2023-06-01 09:01:06 +0200

    start position 2nd day

--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -1,19 +1,19 @@
 import ArtPiecePreview from "../ArtPiecePreview";
 
 export default function ArtPieces({ pieces }) {
-
   return (
-      <ul>
-        {pieces.map((piece) => {
-          return (
-            <ArtPiecePreview
-              key={piece.id}
-              image={piece.imageSource}
-              title={piece.name}
-              artist={piece.artist}
-            />
-          );
-        })}
-      </ul>
-  )
-};
+    <ul>
+      {pieces.map((piece) => {
+        console.log(piece);
+        return (
+          <ArtPiecePreview
+            key={piece.imageSource}
+            image={piece.imageSource}
+            title={piece.name}
+            artist={piece.artist}
+          />
+        );
+      })}
+    </ul>
+  );
+}
