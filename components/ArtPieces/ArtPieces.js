babb05d85eecaefc7040b7c1b7fc905e7d098e3c
kasi <danielkaser@mailbox.org>
2023-09-19 18:40:20 +0200

    remove return from pieces.map

--- components/ArtPieces/ArtPieces.js
+++ components/ArtPieces/ArtPieces.js
@@ -4,19 +4,17 @@ export function ArtPieces({ pieces, handleToggleFavorite, artPiecesInfo }) {
   //
   return (
     <ul>
-      {pieces.map((piece) => {
-        return (
-          <ArtPiecePreview
-            key={piece.imageSource}
-            image={piece.imageSource}
-            title={piece.name}
-            artist={piece.artist}
-            slug={piece.slug}
-            handleToggleFavorite={handleToggleFavorite}
-            artPiecesInfo={artPiecesInfo}
-          />
-        );
-      })}
+      {pieces.map((piece) => (
+        <ArtPiecePreview
+          key={piece.imageSource}
+          image={piece.imageSource}
+          title={piece.name}
+          artist={piece.artist}
+          slug={piece.slug}
+          handleToggleFavorite={handleToggleFavorite}
+          artPiecesInfo={artPiecesInfo}
+        />
+      ))}
     </ul>
   );
 }
