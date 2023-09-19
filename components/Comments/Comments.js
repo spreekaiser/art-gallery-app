a9e4c4d8bfe84a77555e187a521d171c059f270b
kasi <danielkaser@mailbox.org>
2023-09-19 16:21:45 +0200

    clean up comments comp

--- components/Comments/Comments.js
+++ components/Comments/Comments.js
@@ -6,14 +6,9 @@ export function Comments({ title }) {
       defaultValue: {},
     });
 
-  //   const comment = (artPiecesComments[slug] || []).map((comment) => {
-  //     console.log("comment in maping: ", comment);
-  //     return comment;
-  //   });
   return (
     <ul>
       {(artPiecesComments[title] || []).map((comment, index) => {
-        // console.log("comment in maping: ", comment);
         return (
           <li className="commentList" key={index}>
             {comment}
