2f6ce1e7965ed26ebe0cdff903cf966a3a3931cd
kasi <danielkaser@mailbox.org>
2023-09-22 14:35:05 +0200

    refactor and clean up favoriteButton comp

--- components/FavoriteButton/FavoriteButton.js
+++ components/FavoriteButton/FavoriteButton.js
@@ -1,31 +1,18 @@
-// import Image from "next/image";
-import FavoriteIcon from "../../assets/heart.svg";
 import { IconHeart } from "..";
 import styled from "styled-components";
-// import { useImmerLocalStorageState } from "../../lib/hook/useImmerLocalStorageState";
 
-let isFavorite = false;
-
-// console.log(<IconHeart />);
-
-// export default function FavoriteButton({ isFavorite, onToggleFavorite, slug }) {
 export function FavoriteButton({ handleToggleFavorite, slug, artPiecesInfo }) {
   return (
     <>
-      {/* <TestStyledHeart size={24} /> */}
       {artPiecesInfo.includes(slug) ? (
-        // <IconHeart />
-        // <FilledFavoriteIcon
         <HeartIconFilled
-          className="test"
+          className="mandatoryClassName" //!  Styled Comp doesn't work here without className
           size={40}
           onClick={() => handleToggleFavorite(slug)}
         />
       ) : (
-        // <IconHeart />
-        // <UnfilledFavoriteIcon
         <HeartIconUnfilled
-          className="test" // mandatory; doesn't work without className
+          className="mandatoryClassName" //!  Styled Comp doesn't work here without className
           size={40}
           onClick={() => handleToggleFavorite(slug)}
         />
@@ -34,11 +21,11 @@ export function FavoriteButton({ handleToggleFavorite, slug, artPiecesInfo }) {
   );
 }
 
-const HeartIconUnfilled = styled(IconHeart)`
+const StyledHeartIcon = styled(IconHeart)`
   position: absolute;
   top: 25px;
   right: 25px;
-  fill: transparent;
+
   stroke: crimson;
   stroke-width: 0.15rem;
 
@@ -47,67 +34,23 @@ const HeartIconUnfilled = styled(IconHeart)`
     transform: scale(1.2);
     cursor: pointer;
   }
-
   &:active {
     fill: white;
+  }
+`;
+
+const HeartIconUnfilled = styled(StyledHeartIcon)`
+  fill: transparent;
+
+  &:active {
     stroke: white;
   }
 `;
 
-const HeartIconFilled = styled(IconHeart)`
-  position: absolute;
-  top: 25px;
-  right: 25px;
+const HeartIconFilled = styled(StyledHeartIcon)`
   fill: crimson;
-  stroke: crimson;
-  stroke-width: 0.15rem;
-
-  &:hover {
-    fill: crimson;
-    transform: scale(1.2);
-    cursor: pointer;
-  }
 
   &:active {
-    fill: white;
     stroke: crimson;
-  }
-`;
-
-/* const FilledFavoriteIcon = styled(IconHeart)` */
-const FilledFavoriteIcon = styled(FavoriteIcon)`
-  position: absolute;
-  top: 25px;
-  right: 25px;
-  stroke: white;
-  stroke: crimson;
-  stroke-width: 0.2rem;
-  fill: crimson;
-
-  height: 40px;
-  width: 40px;
-
-  &:hover {
-    fill: crimson;
-    cursor: pointer;
-  }
-`;
-
-/* const UnfilledFavoriteIcon = styled(IconHeart)` */
-const UnfilledFavoriteIcon = styled(FavoriteIcon)`
-  position: absolute;
-  top: 25px;
-  right: 25px;
-  stroke: white;
-  stroke: crimson;
-  stroke-width: 0.2rem;
-  fill: transparent;
-
-  height: 40px;
-  width: 40px;
-
-  &:hover {
-    fill: crimson;
-    cursor: pointer;
   }
 `;
