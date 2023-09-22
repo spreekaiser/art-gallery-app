9677e578c397f30718073a5ef6d54c84490d4c60
kasi <danielkaser@mailbox.org>
2023-09-22 15:56:17 +0200

    fix first test for comments comp

--- components/Comments/Comments.test.js
+++ components/Comments/Comments.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 
 test("render a list", () => {
   render(<Comments />);
-  const listItem = screen.getByRole("list", {
+  const list = screen.getByRole("list", {
     title: Comments,
   });
   expect(list).toBeInTheDocument();
