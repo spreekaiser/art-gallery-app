609775fa4308a8ffb6c92b9529c5aa864f50fb1c
kasi <danielkaser@mailbox.org>
2023-09-27 15:10:44 +0200

    add test for art pieces link tobeintheNavbar

--- components/Navigation/Navigation.test.js
+++ components/Navigation/Navigation.test.js
@@ -1,10 +1,15 @@
-// import { ArtPieces } from "..";
 import { render, screen } from "@testing-library/react";
-import "jest-styled-components";
+// import "jest-styled-components";
 import { Navigation } from "..";
 
 test("A navigation link 'Spotlight' is displayed", () => {
   render(<Navigation />);
   const link = screen.getByRole("link", { name: /spotlight/i });
+  expect(link).toBeInTheDocument();
+});
+
+test("A navigation link 'Art Pieces' is displayed", () => {
+  render(<Navigation />);
+  const link = screen.getByRole("link", { name: /art pieces/i });
   expect(link).toBeInTheDocument();
 });
