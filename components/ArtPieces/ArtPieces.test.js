76395974beb917fb0d4833bbdb1147faf153d62d
kasi <danielkaser@mailbox.org>
2023-09-28 18:11:45 +0200

    create tests for titles and images to be in the document

--- components/ArtPieces/ArtPieces.test.js
+++ components/ArtPieces/ArtPieces.test.js
@@ -1,35 +1,50 @@
 import { ArtPieces } from "..";
 import { ArtPieceContainer } from "..";
 import { ArtPiecePreview } from "..";
-import { Navigation } from "..";
 import { render, screen } from "@testing-library/react";
+import { demoPiece } from "../../_testData/testData";
 import "jest-styled-components";
 
-test("All art pieces are displayed as a list", () => {
+it("should be displayed all art pieces as a list", () => {
   render(<ArtPieces />);
   const ulElement = screen.getByRole("list");
   expect(ulElement).toBeInTheDocument();
 });
 
-/*
-test("Each art piece's image is displayed", () => {
-  render(<ArtPieceContainer />);
-  // const image = screen.getByRole("img");
-  const image = screen.getByRole("img", {
+it("should be displayed each art piece's image", () => {
+  const handleToggleFavorite = jest.fn();
+  const artPiecesInfo = [];
+
+  render(
+    <ArtPiecePreview
+      // key={bla.slug}
+      piece={demoPiece}
+      handleToggleFavorite={handleToggleFavorite}
+      artPiecesInfo={artPiecesInfo}
+    />
+  );
+  const imageElement = screen.getByRole("img", {
     name: /Orange Red and Green Abstract Painting/i,
   });
-  expect(image).toBeInTheDocument();
+  expect(imageElement).toBeInTheDocument();
 });
-*/
 
-test("Each art piece's title is displayed", () => {
-  render(<ArtPiecePreview />);
-  const title = screen.getByRole("heading");
-  // const title = screen.getByRole("heading", {
-  // name: /Orange Red and Green Abstract Painting/i,
-  //// name: /bla/i,
-  // });
-  expect(title).toBeInTheDocument();
+it("should be displayed each art piece's title", () => {
+  const handleToggleFavorite = jest.fn();
+  const artPiecesInfo = [];
+
+  render(
+    <ArtPiecePreview
+      // key={bla.slug}
+      piece={demoPiece}
+      handleToggleFavorite={handleToggleFavorite}
+      artPiecesInfo={artPiecesInfo}
+    />
+  );
+  const headerElement = screen.getByRole("heading", {
+    name: /Orange Red and Green Abstract Painting/i,
+  });
+  expect(headerElement).toBeInTheDocument();
 });
 
 // test("teset", () => {
