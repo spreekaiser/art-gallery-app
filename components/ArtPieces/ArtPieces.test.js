9ef95fe58de94865e0c13e034e6690d6bbbd7ce9
kasi <danielkaser@mailbox.org>
2023-09-28 18:17:07 +0200

    create test for author to be in the document

--- components/ArtPieces/ArtPieces.test.js
+++ components/ArtPieces/ArtPieces.test.js
@@ -1,58 +1,65 @@
-import { ArtPieces } from "..";
-import { ArtPieceContainer } from "..";
-import { ArtPiecePreview } from "..";
 import { render, screen } from "@testing-library/react";
-import { demoPiece } from "../../_testData/testData";
 import "jest-styled-components";
+import { ArtPieces } from "..";
+import { ArtPiecePreview } from "..";
+import { demoPiece } from "../../_testData/testData";
 
-it("should be displayed all art pieces as a list", () => {
-  render(<ArtPieces />);
-  const ulElement = screen.getByRole("list");
-  expect(ulElement).toBeInTheDocument();
-});
-
-it("should be displayed each art piece's image", () => {
-  const handleToggleFavorite = jest.fn();
-  const artPiecesInfo = [];
-
-  render(
-    <ArtPiecePreview
-      // key={bla.slug}
-      piece={demoPiece}
-      handleToggleFavorite={handleToggleFavorite}
-      artPiecesInfo={artPiecesInfo}
-    />
-  );
-  const imageElement = screen.getByRole("img", {
-    name: /Orange Red and Green Abstract Painting/i,
+describe("Art Pieces Tests", () => {
+  //
+  it("should be displayed all art pieces as a list", () => {
+    render(<ArtPieces />);
+    const ulElement = screen.getByRole("list");
+    expect(ulElement).toBeInTheDocument();
   });
-  expect(imageElement).toBeInTheDocument();
-});
 
-it("should be displayed each art piece's title", () => {
-  const handleToggleFavorite = jest.fn();
-  const artPiecesInfo = [];
+  it("should be displayed each art piece's image", () => {
+    const handleToggleFavorite = jest.fn();
+    const artPiecesInfo = [];
 
-  render(
-    <ArtPiecePreview
-      // key={bla.slug}
-      piece={demoPiece}
-      handleToggleFavorite={handleToggleFavorite}
-      artPiecesInfo={artPiecesInfo}
-    />
-  );
-  const headerElement = screen.getByRole("heading", {
-    name: /Orange Red and Green Abstract Painting/i,
+    render(
+      <ArtPiecePreview
+        piece={demoPiece}
+        handleToggleFavorite={handleToggleFavorite}
+        artPiecesInfo={artPiecesInfo}
+      />
+    );
+    const imageElement = screen.getByRole("img", {
+      name: /Orange Red and Green Abstract Painting/i,
+    });
+    expect(imageElement).toBeInTheDocument();
   });
-  expect(headerElement).toBeInTheDocument();
+
+  it("should be displayed each art piece's title", () => {
+    const handleToggleFavorite = jest.fn();
+    const artPiecesInfo = [];
+
+    render(
+      <ArtPiecePreview
+        piece={demoPiece}
+        handleToggleFavorite={handleToggleFavorite}
+        artPiecesInfo={artPiecesInfo}
+      />
+    );
+    const headerElement = screen.getByRole("heading", {
+      name: /Orange Red and Green Abstract Painting/i,
+    });
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  it("should be displayed each art piece's artist", () => {
+    const handleToggleFavorite = jest.fn();
+    const artPiecesInfo = [];
+
+    render(
+      <ArtPiecePreview
+        piece={demoPiece}
+        handleToggleFavorite={handleToggleFavorite}
+        artPiecesInfo={artPiecesInfo}
+      />
+    );
+    const headerElement = screen.getByRole("heading", {
+      name: /Steve Johnson/i,
+    });
+    expect(headerElement).toBeInTheDocument();
+  });
 });
-
-// test("teset", () => {
-//   render(<Navigation />);
-//   const link = screen.getByRole("link", { name: /spotligh/i });
-//   expect(link).toBeInTheDocument();
-// });
-
-// test("Each art piece's artist is displayed", () => {
-//   render(<ArtPieces />);
-// });
