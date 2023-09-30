cb8c9c5ecf1c6285aa5ae87bc81d2d2ed35821e5
kasi <danielkaser@mailbox.org>
2023-09-30 17:58:28 +0200

    create test for favorite button to be displayed in the spotlight view

--- 2do.md
+++ 2do.md
@@ -163,7 +163,7 @@ Download and extract the [Resources](#resources) into your project folder.
 
 #### Acceptance Criteria
 
-- [ ] The favorite-button is displayed in the spotlight view 🖼️
+- [x] The favorite-button is displayed in the spotlight view 🖼️
 - [ ] The favorite-button is displayed in each entry in the list view 🖼️
 - [ ] The favorite-button is displayed in the details view 🖼️
 - [ ] Clicking the favorite-button on a non-favorite piece saves it as a favorite 🖼️
--- components/FavoriteButton/FavoriteButton.test.js
+++ components/FavoriteButton/FavoriteButton.test.js
@@ -1,8 +1,23 @@
 import { screen, render } from "@testing-library/react";
+import { SpotLight } from "..";
+import { demoPieces } from "../../_testData/testData";
 
 describe("FavoriteButton Integration-Tests", () => {
   //
-  it("should display the favorite-button in the spotlight view", () => {});
+  it("should display the favorite-button in the spotlight view", () => {
+    const mockArtPiecesInfo = [];
+    const mockHandleToggleFavorite = jest.fn();
+    render(
+      <SpotLight
+        pieces={demoPieces}
+        artPiecesInfo={mockArtPiecesInfo}
+        handleToggleFavorite={mockHandleToggleFavorite}
+      />
+    );
+    const svgElement = screen.getByTestId("favorite-button");
+    expect(svgElement).toBeInTheDocument();
+  });
+
   it("should display the favorite-button in each entry in the list view", () => {});
   it("should display the favorite-button in the details view", () => {});
   it("should save a non-favorite piece as favorite on click", () => {});
