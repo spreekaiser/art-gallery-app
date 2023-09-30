c95330ff74cc6e1f3c46f276fee7556016024277
kasi <danielkaser@mailbox.org>
2023-09-30 17:33:53 +0200

    create favoriteButton test file and prepare tests

---
+++ components/FavoriteButton/FavoriteButton.test.js
@@ -0,0 +1,10 @@
+import { screen, render } from "@testing-library/react";
+
+describe("FavoriteButton Integration-Tests", () => {
+  //
+  it("should display the favorite-button in the spotlight view", () => {});
+  it("should display the favorite-button in each entry in the list view", () => {});
+  it("should display the favorite-button in the details view", () => {});
+  it("should save a non-favorite piece as favorite on click", () => {});
+  it("should remove a favorite piece from favorites on click", () => {});
+});
