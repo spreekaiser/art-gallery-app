9a5439706d58dc41d0e6fd5303f8a521ec3b1504
kasi <danielkaser@mailbox.org>
2023-10-06 16:09:09 +0200

    create test for removing piece from favorites on second click of favorite button

--- components/FavoriteButton/FavoriteButton.test.js
+++ components/FavoriteButton/FavoriteButton.test.js
@@ -3,7 +3,7 @@ import { ArtPieceDetails, ArtPieces, FavoriteButton, SpotLight } from "..";
 import { demoPieces } from "../../_testData/testData";
 
 const mockHandleToggleFavorite = jest.fn();
-const mockArtPiecesInfo = [];
+let mockArtPiecesInfo = [];
 
 describe("FavoriteButton Integration-Tests", () => {
   //
@@ -51,14 +51,43 @@ describe("FavoriteButton Integration-Tests", () => {
         slug={demoPieces[0].slug}
       />
     );
-
     const svgElement = screen.getByTestId("favorite-button");
     fireEvent.click(svgElement);
     mockArtPiecesInfo.push(demoPieces[0].slug);
-
     expect(mockHandleToggleFavorite).toHaveBeenCalledTimes(1);
     expect(mockArtPiecesInfo.includes(demoPieces[0].slug)).toBeTruthy();
   });
 
-  // it("should remove a favorite piece from favorites on click", () => {});
+  // it("should render filled heart icon styled component", async () => {
+  //   render(
+  //     <FavoriteButton
+  //       handleToggleFavorite={mockHandleToggleFavorite}
+  //       artPiecesInfo={mockArtPiecesInfo}
+  //       slug={demoPieces[0].slug}
+  //     />
+  //   );
+  //   const svgElement = screen.getByTestId("favorite-button");
+  //   // await fireEvent.click(svgElement);
+  //   await userEvent.click(svgElement);
+  //   screen.debug();
+  //   expect(svgElement).toHaveClass("favorite-active");
+  // });
+  //
+  it("should remove a favorite piece from favorites on click", () => {
+    render(
+      <FavoriteButton
+        handleToggleFavorite={mockHandleToggleFavorite}
+        artPiecesInfo={mockArtPiecesInfo}
+        slug={demoPieces[0].slug}
+      />
+    );
+    const svgElement = screen.getByTestId("favorite-button");
+    fireEvent.click(svgElement);
+
+    mockArtPiecesInfo = mockArtPiecesInfo.filter(
+      (slug) => slug !== demoPieces[0].slug
+    );
+    expect(mockHandleToggleFavorite).toHaveBeenCalledTimes(2);
+    expect(mockArtPiecesInfo.includes(demoPieces[0].slug)).toBeFalsy();
+  });
 });
