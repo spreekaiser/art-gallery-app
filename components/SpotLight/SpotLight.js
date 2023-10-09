9beb262a1f122b7ffc60db675fb05da7b97847bd
kasi <danielkaser@mailbox.org>
2023-10-09 14:00:04 +0200

    create useGetRandomPieceOnIntervall hook in spotlight component

--- components/SpotLight/SpotLight.js
+++ components/SpotLight/SpotLight.js
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import { ArtPiecePreview } from "..";
 import { getRandomNumber } from "../../lib/utils";
 
-export function SpotLight({ pieces, handleToggleFavorite, artPiecesInfo }) {
-  const [randomIndex, setRandomIndex] = useState(getRandomNumber(pieces));
-
+export function useGetRandomPieceOnIntervall(
+  pieces,
+  getRandomNumber,
+  setRandomIndex
+) {
   useEffect(() => {
     const intervalID = setInterval(() => {
       setRandomIndex(getRandomNumber(pieces));
@@ -13,6 +15,21 @@ export function SpotLight({ pieces, handleToggleFavorite, artPiecesInfo }) {
       clearInterval(intervalID);
     };
   });
+}
+
+export function SpotLight({ pieces, handleToggleFavorite, artPiecesInfo }) {
+  const [randomIndex, setRandomIndex] = useState(getRandomNumber(pieces));
+
+  // useEffect(() => {
+  //   const intervalID = setInterval(() => {
+  //     setRandomIndex(getRandomNumber(pieces));
+  //   }, 3000);
+  //   return () => {
+  //     clearInterval(intervalID);
+  //   };
+  // });
+
+  useGetRandomPieceOnIntervall(pieces, getRandomNumber, setRandomIndex);
 
   const piece = pieces[randomIndex];
 
