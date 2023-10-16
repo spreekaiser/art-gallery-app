84accc9a37a66d272689aad8340bc834c7fe177f
kasi <danielkaser@mailbox.org>
2023-10-16 17:00:03 +0200

    style background heart-blob

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import { Navigation } from "..";
+import HeartBlob from "../../assets/heart-blob.svg";
 
 export function Layout({ children }) {
   return (
     <>
       <Content>{children}</Content>
+      <StyledHeartBlob />
       <Navigation />
     </>
   );
@@ -15,4 +17,16 @@ const Content = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 10%;
+`;
+
+const StyledHeartBlob = styled(HeartBlob)`
+  position: fixed;
+  top: 40%;
+  left: 70%;
+  transform: translate(-50%, -50%);
+  z-index: -100;
+  height: 30rem;
+  width: 30rem;
+  rotate: 10deg;
+  fill: yellow;
 `;
