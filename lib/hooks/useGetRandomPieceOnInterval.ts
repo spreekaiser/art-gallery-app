7c20bc08e7b2ad499bfbf2cb3061aae37c4ddfb0
kasi <danielkaser@mailbox.org>
2023-10-20 16:35:48 +0200

    rename custom hook useGetRandomPieceOnInterval.js to .ts and add types

--- lib/hooks/useGetRandomPieceOnInterval.ts
+++ lib/hooks/useGetRandomPieceOnInterval.ts
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { SetStateAction, useEffect } from "react";
+import { PieceType } from "../utils.d";
 
 export default function useGetRandomPieceOnInterval(
-  pieces,
-  getRandomNumber,
-  setRandomIndex
+  pieces: PieceType[],
+  getRandomNumber: (pieces: PieceType[]) => number,
+  setRandomIndex: React.Dispatch<SetStateAction<number>>
 ) {
   useEffect(() => {
     const intervalID = setInterval(() => {
