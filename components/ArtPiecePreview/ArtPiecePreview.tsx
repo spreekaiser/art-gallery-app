76cda99b453a870b8c7028ea19b665b6c84320d9
kasi <danielkaser@mailbox.org>
2023-10-23 17:51:16 +0200

    rename ArtPiecePreview.js to .tsx and add types/interface to it

--- components/ArtPiecePreview/ArtPiecePreview.tsx
+++ components/ArtPiecePreview/ArtPiecePreview.tsx
@@ -1,11 +1,32 @@
 import styled from "styled-components";
-import { ArtPieceContainer } from "../";
+import { ArtPieceContainer } from "..";
+
+interface IPiece {
+  slug: string;
+  artist: string;
+  name: string;
+  imageSource: string;
+  year: string;
+  genre: string;
+  colors: string[];
+  dimensions: {
+    height: number;
+    width: number;
+    type: string;
+  };
+}
+
+type ArtPiecePreviewProps = {
+  piece: IPiece;
+  handleToggleFavorite: (slug: string) => {};
+  artPiecesInfo: string[];
+};
 
 export function ArtPiecePreview({
   piece,
   handleToggleFavorite,
   artPiecesInfo,
-}) {
+}: ArtPiecePreviewProps) {
   return (
     <StyledListItem>
       <h3>{piece.name}</h3>
