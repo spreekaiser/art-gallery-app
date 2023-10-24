7bfeb6557c1499201fe2d5c923fbad0607d15f38
kasi <danielkaser@mailbox.org>
2023-10-24 16:30:02 +0200

    rename SpotLight.js to .tsx and create props type

--- components/SpotLight/SpotLight.tsx
+++ components/SpotLight/SpotLight.tsx
@@ -2,8 +2,19 @@ import { useState } from "react";
 import { ArtPiecePreview } from "..";
 import { getRandomNumber } from "../../lib/utils";
 import useGetRandomPieceOnInterval from "../../lib/hooks/useGetRandomPieceOnInterval";
+import { IPiece } from "../../types";
 
-export function SpotLight({ pieces, handleToggleFavorite, artPiecesInfo }) {
+type SpotLightProps = {
+  pieces: IPiece[];
+  handleToggleFavorite: (slug: string) => {};
+  artPiecesInfo: string[];
+};
+
+export function SpotLight({
+  pieces,
+  handleToggleFavorite,
+  artPiecesInfo,
+}: SpotLightProps) {
   const [randomIndex, setRandomIndex] = useState(getRandomNumber(pieces));
 
   useGetRandomPieceOnInterval(pieces, getRandomNumber, setRandomIndex);
