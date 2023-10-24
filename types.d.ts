f86235865db53cf729777f8031af1bbf68fccad2
kasi <danielkaser@mailbox.org>
2023-10-24 16:20:33 +0200

    create global types file and create IPiece interface there

---
+++ types.d.ts
@@ -0,0 +1,14 @@
+export interface IPiece {
+  slug: string;
+  artist: string;
+  name: string;
+  imageSource: string;
+  year: string;
+  genre: string;
+  colors: string[];
+  dimensions: {
+    height: number;
+    width: number;
+    type: string;
+  };
+}
