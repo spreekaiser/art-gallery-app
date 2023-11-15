e01f7142e4eedab647f008b95e5edf3657856b0e
kasi <danielkaser@mailbox.org>
2023-11-15 19:30:06 +0100

    rename index.js to .tsx and add prop types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import { SpotLight } from "../components";
+import { ArtPiecesProps as Props } from "../types";
 
 export default function SpotlightPage({
   pieces,
   handleToggleFavorite,
   artPiecesInfo,
-}) {
+}: Props) {
   return (
     <>
       <SpotLight
