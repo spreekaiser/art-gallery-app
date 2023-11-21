587f538f63171dc584ca03720c1933da6aa7df5d
kasi <danielkaser@mailbox.org>
2023-11-21 19:01:03 +0100

    move loadingspinner styles to seperate file

--- components/LoadingSpinner/LoadingSpinner.tsx
+++ components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import styled, { keyframes } from "styled-components";
+import { StyledSpinnerContainer, StyledSpinner } from "./LoadingSpinner.style";
 
 export function LoadingSpinner() {
   return (
@@ -8,34 +7,3 @@ export function LoadingSpinner() {
     </StyledSpinnerContainer>
   );
 }
-
-export const spin = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
-
-export const StyledSpinnerContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: white;
-`;
-
-export const StyledSpinner = styled.div`
-  width: 3rem;
-  height: 3rem;
-  border-width: 0.3rem;
-  border-style: solid;
-  border-image: linear-gradient(
-      to top,
-      var(--accent-color1),
-      var(--accent-color2)
-    )
-    1;
-  animation: ${spin} 1.5s linear infinite;
-`;
