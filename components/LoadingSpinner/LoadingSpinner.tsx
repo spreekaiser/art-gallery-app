82ee66a71e69b87f83b54b59d418a9d3de308c29
kasi <danielkaser@mailbox.org>
2023-11-21 18:56:23 +0100

    restyle loadingspinner to gradient square

--- components/LoadingSpinner/LoadingSpinner.tsx
+++ components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import "./LoadingSpinner.css";
 import styled, { keyframes } from "styled-components";
 
 export function LoadingSpinner() {
@@ -24,32 +23,19 @@ export const StyledSpinnerContainer = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
+  background-color: white;
 `;
 
 export const StyledSpinner = styled.div`
-  /* border: 4px solid rgba(0, 0, 0, 0.1); */
-  /* border-top: 4px solid #767676; Farbe des Spinners */
-
-  /* border: 4px solid rgba(0, 0, 0, 0.1);
-  border-top: 4px solid var(--accent-color1);
-  border-left: 4px solid var(--accent-color1);
-  border-right: 4px solid var(--accent-color1);
-
-  border-radius: 50%;
-  width: 50px; 
-  height: 50px; */
-
-  /* animation: ${spin} 4s linear infinite; */
-
-  width: 50px;
-  height: 50px;
-  border: 4px solid transparent;
-  border-top: 4px solid var(--accent-color1);
-  border-left: 4px solid var(--accent-color1);
-  border-bottom: 4px solid var(--accent-color1);
-  /* border-right: none;  */
-  border-right: 4px solid transparent;
-  border-radius: 50%;
-  animation: ${spin} 1s linear infinite; /* Verwende die erstellten Keyframes */
-  transform: rotate(45deg); /* Rotation */
+  width: 3rem;
+  height: 3rem;
+  border-width: 0.3rem;
+  border-style: solid;
+  border-image: linear-gradient(
+      to top,
+      var(--accent-color1),
+      var(--accent-color2)
+    )
+    1;
+  animation: ${spin} 1.5s linear infinite;
 `;
