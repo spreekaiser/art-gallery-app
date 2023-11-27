62135b7bcdd3b627847fdacb312c37406277410b
kasi <danielkaser@mailbox.org>
2023-11-27 16:35:41 +0100

    move comment form styles to commentform.styles

--- components/CommentForm/CommentForm.tsx
+++ components/CommentForm/CommentForm.tsx
@@ -1,9 +1,14 @@
-import styled from "styled-components";
 import { useImmerLocalStorageState } from "../../lib/hooks/useImmerLocalStorageState";
 import {
   ICommentFormProps as IProps,
   IArtPiecesComments,
 } from "./CommentForm.d";
+import {
+  StyledForm,
+  StyledFieldset,
+  StyledTextarea,
+  StyledButton,
+} from "./CommentForm style";
 
 export function CommentForm({ title }: IProps) {
   const [artPiecesComments, updateArtPiecesComments]: [
@@ -37,50 +42,10 @@ export function CommentForm({ title }: IProps) {
         <StyledTextarea
           id="commentInput"
           name="commentInput"
-          placeholder="Add a free comment..."
+          placeholder="Add a comment... it's free!"
         ></StyledTextarea>
         <StyledButton type="submit">Submit</StyledButton>
       </StyledFieldset>
     </StyledForm>
   );
 }
-
-const StyledForm = styled.form`
-  width: 100%;
-  margin: 1rem 0;
-`;
-
-const StyledFieldset = styled.fieldset`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-
-  width: inherit;
-  padding: 0;
-  border: none;
-`;
-
-const StyledTextarea = styled.textarea`
-  width: inherit;
-  padding: 1rem;
-  border-radius: 5px;
-`;
-
-const StyledButton = styled.button`
-  background-color: var(--accent-color1);
-  color: var(--main-color2);
-  font-weight: bold;
-  padding: 0.5rem 1rem;
-  border-radius: 5px;
-
-  transition: all 0.3s ease;
-
-  &:hover {
-    filter: brightness(110%);
-    color: var(--accent-color2);
-    cursor: pointer;
-    box-shadow: 3px 3px 6px #333;
-    transform: scale(1.02) rotate(1deg);
-  }
-`;
