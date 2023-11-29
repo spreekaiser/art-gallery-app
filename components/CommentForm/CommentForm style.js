64545b82f9c1755632c3c98609d992fc8deac1ed
kasi <danielkaser@mailbox.org>
2023-11-29 13:10:34 +0100

    add styles and styles logic for disabled submit button

--- components/CommentForm/CommentForm style.js
+++ components/CommentForm/CommentForm style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const StyledForm = styled.form`
   width: 100%;
@@ -31,11 +31,17 @@ export const StyledButton = styled.button`
 
   transition: all 0.3s ease;
 
-  &:hover {
-    filter: brightness(110%);
-    color: var(--accent-color2);
-    cursor: pointer;
-    box-shadow: 3px 3px 6px #333;
-    transform: scale(1.02) rotate(1deg);
-  }
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  filter: ${(props) => (props.disabled ? "saturate(50%)" : "none")};
+
+  ${(props) =>
+    !props.disabled &&
+    css`
+      &:hover {
+        filter: brightness(110%);
+        color: var(--accent-color2);
+        box-shadow: 3px 3px 6px #333;
+        transform: scale(1.02) rotate(1deg);
+      }
+    `}
 `;
