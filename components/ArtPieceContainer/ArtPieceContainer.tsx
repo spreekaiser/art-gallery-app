9dc0cd8db0ed3523a3672b6e5397737d337325bf
kasi <danielkaser@mailbox.org>
2023-11-29 13:58:58 +0100

    move link from images to dedicated link under the image

--- components/ArtPieceContainer/ArtPieceContainer.tsx
+++ components/ArtPieceContainer/ArtPieceContainer.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { FavoriteButton } from "..";
 import { ArtPieceContainerProps as Props } from "./ArtPieceContainer.d";
+import styled from "styled-components";
 
 export function ArtPieceContainer({
   artist,
@@ -14,9 +15,9 @@ export function ArtPieceContainer({
   return (
     <>
       <div className="favoriteDiv">
-        <Link href={`art-pieces/${slug}`}>
-          <Image src={imageURL} alt={name} width={360} height={240} />
-        </Link>
+        {/* <Link href={`art-pieces/${slug}`}> */}
+        <Image src={imageURL} alt={name} width={360} height={240} />
+        {/* </Link> */}
         <FavoriteButton
           slug={slug}
           handleToggleFavorite={handleToggleFavorite}
@@ -24,6 +25,16 @@ export function ArtPieceContainer({
         />
       </div>
       <h4>by {artist}</h4>
+      <StyledLink href={`art-pieces/${slug}`}>Take a closer look</StyledLink>
     </>
   );
 }
+
+const StyledLink = styled(Link)`
+  margin-bottom: 1rem;
+
+  &:hover {
+    color: var(--main-color2);
+    font-weight: bold;
+  }
+`;
