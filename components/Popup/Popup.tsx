d752b9d07c282eab08588a856794cc27e1e0f0a6
kasi <danielkaser@mailbox.org>
2023-12-06 16:26:48 +0100

    add basic styling for intro popup

--- components/Popup/Popup.tsx
+++ components/Popup/Popup.tsx
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 interface IProps {
   trigger: string;
   delay: number;
   content: string;
   buttonText: string;
+  watchman: "sleepy" | "angry" | "concerned";
 }
 
-export function Popup({ trigger, delay, content, buttonText }: IProps) {
+export function Popup({
+  trigger,
+  delay,
+  content,
+  watchman,
+  buttonText,
+}: IProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
@@ -23,15 +31,54 @@ export function Popup({ trigger, delay, content, buttonText }: IProps) {
   }, [trigger, delay]);
 
   return (
-    <div>
+    <>
       {isOpen && (
-        <div className="popup">
-          <div className="popup-content">
-            <p>{content}</p>
-            <button onClick={() => setIsOpen(false)}>{buttonText}</button>
-          </div>
-        </div>
+        <PopupBox className="popup">
+          {/* <div className="popup-content"> */}
+          <Text>{content}</Text>
+          <StyledButton onClick={() => setIsOpen(false)}>
+            {buttonText}
+          </StyledButton>
+          {/* </div> */}
+        </PopupBox>
       )}
-    </div>
+    </>
   );
 }
+
+export const PopupBox = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 2rem;
+  background-color: var(--main-color2);
+  z-index: 1000;
+
+  border: 0.3rem solid ${(props) => props.color};
+  border-radius: 2rem;
+  border-bottom-right-radius: 0;
+  /* height: 50vh; */
+  width: 50vw;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+// textbox style
+export const Text = styled.p`
+  font-size: 1.5rem;
+  text-align: center;
+`;
+
+export const StyledButton = styled.button`
+  &:hover {
+    filter: brightness(110%);
+    color: var(--accent-color2);
+    cursor: pointer;
+    box-shadow: 3px 3px 6px #333;
+    transform: scale(1.02) rotate(1deg);
+  }
+`;
