b3dd491b4ea6eeb9e29af4d3397c68d4873e1f0c
kasi <danielkaser@mailbox.org>
2023-12-06 17:01:03 +0100

    move Popup styles to Popup.style.js

--- components/Popup/Popup.tsx
+++ components/Popup/Popup.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import { PopupBox, StyledButton, Text } from "./Popup.style";
 
 interface IProps {
   trigger: "load" | "hover" | "touch";
@@ -44,38 +44,3 @@ export function Popup({
     </>
   );
 }
-
-export const PopupBox = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 2rem;
-  background-color: var(--main-color2);
-  z-index: 1000;
-
-  border: 0.3rem solid ${(props) => props.color};
-  border-radius: 2rem;
-  border-bottom-right-radius: 0;
-  width: 50vw;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Text = styled.p`
-  font-size: 1.5rem;
-  text-align: center;
-`;
-
-export const StyledButton = styled.button`
-  &:hover {
-    filter: brightness(110%);
-    color: var(--accent-color2);
-    cursor: pointer;
-    box-shadow: 3px 3px 6px #333;
-    transform: scale(1.02) rotate(1deg);
-  }
-`;
