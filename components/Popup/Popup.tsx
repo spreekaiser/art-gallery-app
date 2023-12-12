344ba714eb2099fc681985f82a9b10ffccf50fcb
kasi <danielkaser@mailbox.org>
2023-12-12 14:52:23 +0100

    clean up Popup.tsx

--- components/Popup/Popup.tsx
+++ components/Popup/Popup.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useMemo } from "react";
 import { IPopupProps as IProps } from "./Popup.d";
 import { PopupBox, StyledButton, Text } from "./Popup.style";
 import { popupData } from "./utils/popupData";
@@ -8,21 +8,9 @@ export function Popup({ trigger }: IProps) {
   const popupContents = useMemo(() => popupData, []);
 
   const [isOpen, setIsOpen] = useState(false);
-  const { content, delay, watchman, button } = popupContents[trigger];
+  const { content, button } = popupContents[trigger];
 
   useOpenPopupOnInterval(popupContents, trigger, setIsOpen);
-
-  // useEffect(() => {
-  //   if (popupContents[trigger]) {
-  //     const timeout = setTimeout(() => {
-  //       setIsOpen(true);
-  //     }, delay || 0);
-
-  //     return () => {
-  //       clearTimeout(timeout);
-  //     };
-  //   }
-  // }, [trigger, delay, popupContents]);
 
   return (
     <>
