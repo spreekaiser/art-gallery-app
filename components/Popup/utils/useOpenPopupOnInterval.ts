6d72ccc14d3974452ecaffdc460feeeb45874842
kasi <danielkaser@mailbox.org>
2023-12-12 15:02:23 +0100

    clean up useOpenPopupOnInterval hook

--- components/Popup/utils/useOpenPopupOnInterval.ts
+++ components/Popup/utils/useOpenPopupOnInterval.ts
@@ -1,16 +1,12 @@
 import { Dispatch, SetStateAction, useEffect } from "react";
-import { IPopupContents, IPopupData, TriggerType } from "../Popup.d";
+import { IPopupData } from "../Popup.d";
 
 export default function useOpenPopupOnInterval(
-  //   popupContent: IPopupContents,
   popupContent: IPopupData,
-  //   trigger: TriggerType,
   setIsOpen: Dispatch<SetStateAction<boolean>>
 ) {
   useEffect(() => {
-    // if (popupContents[trigger]) {
     if (popupContent) {
-      //   const { delay } = popupContents[trigger];
       const { delay } = popupContent;
       const timeout = setTimeout(() => {
         setIsOpen(true);
