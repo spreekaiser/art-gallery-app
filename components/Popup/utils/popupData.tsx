fea5de0561ecb5694d20b10a32ed10ae3e3aa9eb
kasi <danielkaser@mailbox.org>
2024-01-12 17:25:01 +0100

    add pissed watchman gif

--- components/Popup/utils/popupData.tsx
+++ components/Popup/utils/popupData.tsx
@@ -20,7 +20,7 @@ export const popupData: IPopupContents = {
     button: "Huh?",
   },
   touch: {
-    content: <span>OK buddy I warned you, S.W.A.T. is on their way!</span>,
+    content: <span>OK buddy, I warned you, S.W.A.T. is on their way!</span>,
     delay: 2000,
     watchman: "pissed",
     button: "It wasn't me!",
