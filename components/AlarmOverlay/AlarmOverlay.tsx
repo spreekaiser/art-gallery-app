1ff10b0f7fcc5cd1587a51715c0bcf2d6658ecc7
kasi <danielkaser@mailbox.org>
2024-01-12 17:41:05 +0100

    rename playSound() to playAhoohaAlarm()

--- components/AlarmOverlay/AlarmOverlay.tsx
+++ components/AlarmOverlay/AlarmOverlay.tsx
@@ -1,14 +1,14 @@
 import { Overlay } from "./AlarmOverlay.style";
 import { IAlarmOverlayProps as IProps } from "./AlarmOverlay.d";
 import useToggleAlarmOverlayOnInterval from "./utils/useToggleAlarmOverlayOnInterval";
-import { playSound } from "./utils/audio";
+import { playAhoogaAlarm } from "./utils/audio";
 import { toggleOverlayColor } from "./utils/otherUtils";
 import { Popup } from "..";
 
 export function AlarmOverlay({ isAlarm, setIsAlarm }: IProps) {
   const [overlayColor, boxOverlayColor] = useToggleAlarmOverlayOnInterval(
     isAlarm,
-    playSound,
+    playAhoogaAlarm,
     toggleOverlayColor
   );
 
--- components/AlarmOverlay/utils/audio.ts
+++ components/AlarmOverlay/utils/audio.ts
@@ -1,5 +1,6 @@
 import ahoogaAlarm from "../../../public/audio/ahooga.mp3";
 
-export function playSound(): void {
+// export function playSound(): void {
+export function playAhoogaAlarm(): void {
   new Audio(ahoogaAlarm).play();
 }
