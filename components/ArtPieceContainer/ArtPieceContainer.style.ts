b11852371acd78f56e1893061ce0512d7839cd50
kasi <danielkaser@mailbox.org>
2024-01-15 14:23:41 +0100

    add logic for hinge effect to ArtPieceContainer.style; rename file from .js to .ts

---
+++ components/ArtPieceContainer/ArtPieceContainer.style.ts
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import Link from "next/link";
+import styled, { keyframes } from "styled-components";
+
+interface IStyledImage {
+  clicked: boolean;
+  scrollable?: boolean;
+}
+
+// export const StyledImage = styled(Image)<IStyledImage>`
+export const StyledImage = styled(Image)`
+  border: 20px inset darkgoldenrod;
+  border-radius: 5px;
+  box-shadow: 10px 10px 20px #333;
+
+  &:hover {
+    cursor: grab;
+  }
+  &:active {
+    cursor: grabbing;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  margin-bottom: 1rem;
+
+  &:hover {
+    color: var(--main-color2);
+    font-weight: bold;
+  }
+`;
+
+//
+const hingeAnimation = keyframes`
+  0% {
+    transform-origin: top left;
+    animation-timing-function: ease-in-out;
+  }
+
+  20%, 60% {
+    transform: rotate(80deg);
+    transform-origin: top left;
+    animation-timing-function: ease-in-out;
+  }
+
+  40%, 80% {
+    transform: rotate(60deg);
+    transform-origin: top left;
+    animation-timing-function: ease-in-out;
+  }
+
+  100% {
+    /* transform: rotate(60deg) translate(0, 500px); */
+    transform: translateY(600px) rotate(80deg);
+    transform-origin: top left;
+    animation-timing-function: ease-in;
+  }
+
+`;
+
+const fadeInFromBottom = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const HingeAnimation = styled.div<IStyledImage>`
+  animation: ${(props) => (props.clicked ? hingeAnimation : "none")} 3s ease-out
+    1;
+  transform-origin: top left;
+`;
+
+// export const ReappearAnimation = styled.div`
+//   animation: ${fadeInFromBottom} 1s ease-out;
+// `;
