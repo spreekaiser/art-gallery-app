902d5101a3fc0688389c7fde1e856e552b5673d2
kasi <danielkaser@mailbox.org>
2024-01-22 14:47:14 +0100

    handle alarm state in AlarmOverlay with redux

--- components/AlarmOverlay/AlarmOverlay.tsx
+++ components/AlarmOverlay/AlarmOverlay.tsx
@@ -1,16 +1,22 @@
 import { Overlay } from "./AlarmOverlay.style";
-import { IAlarmOverlayProps as IProps } from "./AlarmOverlay.d";
+//// import { IAlarmOverlayProps as IProps } from "./AlarmOverlay.d";
 import useToggleAlarmOverlayOnInterval from "./utils/useToggleAlarmOverlayOnInterval";
 import { playAhoogaAlarm } from "./utils/audio";
 import { toggleOverlayColor } from "./utils/otherUtils";
 import { Popup } from "..";
+import { useAppSelector } from "../../lib/hooks/storeHooks";
+import { selectAlarm } from "../../store/alarm/alarmSlice";
 
-export function AlarmOverlay({ isAlarm, setIsAlarm }: IProps) {
+//// export function AlarmOverlay({ isAlarm, setIsAlarm }: IProps) {
+export function AlarmOverlay() {
+  const isAlarm = useAppSelector(selectAlarm);
   const [overlayColor, boxOverlayColor] = useToggleAlarmOverlayOnInterval(
     isAlarm,
     playAhoogaAlarm,
     toggleOverlayColor
   );
+
+  // console.log(isAlarm);
 
   if (isAlarm)
     return (
@@ -18,7 +24,7 @@ export function AlarmOverlay({ isAlarm, setIsAlarm }: IProps) {
         <Popup
           trigger="touch"
           borderColor={boxOverlayColor}
-          setIsAlarm={setIsAlarm}
+          //// setIsAlarm={setIsAlarm}
         />
         <Overlay color={overlayColor} />
       </>
