25f62d4400fb12d9992290e1f9dbb409111f7af6
kasi <danielkaser@mailbox.org>
2024-01-23 12:28:49 +0100

    refactor handleTouch() to work with global touchedPiece state

--- utils/handleTouch.ts
+++ utils/handleTouch.ts
@@ -1,12 +1,17 @@
-import { Dispatch, SetStateAction } from "react";
-import { ActionCreatorWithoutPayload } from "@reduxjs/toolkit";
+import {
+  ActionCreatorWithoutPayload,
+  ActionCreatorWithPayload,
+} from "@reduxjs/toolkit";
 import { AppDispatch } from "../store";
 
 export function handleTouch(
   slug: string,
   dispatch: AppDispatch,
   startAlarm: ActionCreatorWithoutPayload<"alarm/startAlarm">,
-  setTouchedArtPiece: Dispatch<SetStateAction<string | null>>
+  setTouchedPiece: ActionCreatorWithPayload<
+    string,
+    "touchedPiece/setTouchedPiece"
+  >
 ): void {
   // if no scrollbar visible, prevent from showing one during animation
   const isScrollbarVisible =
@@ -16,6 +21,5 @@ export function handleTouch(
   }
 
   dispatch(startAlarm());
-
-  setTouchedArtPiece(slug);
+  dispatch(setTouchedPiece(slug));
 }
