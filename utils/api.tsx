520f7e580b782ce1b49964f17878a247deec8bcc
kasi <danielkaser@mailbox.org>
2024-01-24 17:12:46 +0100

    undo global store for fetched data; create cusomt useSWR hook

--- utils/api.tsx
+++ utils/api.tsx
@@ -1,7 +1,18 @@
+import useSWR from "swr";
 import { IApiError } from "../types/types";
 
 export const fetcher = (...args: Parameters<typeof fetch>) =>
   fetch(...args).then((res) => res.json());
+
+export function useFetchData(url: string) {
+  const { data, error, isLoading } = useSWR(url, fetcher);
+
+  if (error) {
+    throw new Error(`Error fetching the data from ${url}`);
+  }
+
+  return { data, error, isLoading };
+}
 
 export function renderErrorMessage(error: IApiError, dataSource: string) {
   return (
