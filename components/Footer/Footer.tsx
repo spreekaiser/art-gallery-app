07b2b80b5b649c0c071da2ec99c8ebad3770fb41
kasi <danielkaser@mailbox.org>
2024-01-25 13:46:07 +0100

    move Footer styles to Footer.styles.js

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,32 +1,11 @@
-import Link from "next/link";
-import styled from "styled-components";
+import { FooterContainer, FooterLink } from "./Footer.style";
 
 export function Footer() {
   return (
-    <StyledDiv>
-      <StyledLink href="/imprint">Imprint</StyledLink>
+    <FooterContainer>
+      <FooterLink href="/imprint">Imprint</FooterLink>
       <span>Made with 🍕 in Kreuzkölln</span>
-      <StyledLink href="/privacy">Privacy</StyledLink>
-    </StyledDiv>
+      <FooterLink href="/privacy">Privacy</FooterLink>
+    </FooterContainer>
   );
 }
-
-const StyledLink = styled(Link)`
-  color: black;
-`;
-
-const StyledDiv = styled.div`
-  z-index: 999;
-  position: fixed;
-  bottom: 0;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  color: var(--main-color2);
-  background-color: var(--accent-color1);
-  text-align: center;
-  width: 100%;
-
-  padding: 1rem 2.5rem;
-  gap: 1rem;
-`;
