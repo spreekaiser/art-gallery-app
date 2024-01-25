8dc2d788a7efbfdec4d2bdb4fe03e5adbd8f00bd
kasi <danielkaser@mailbox.org>
2024-01-25 13:28:21 +0100

    add styled component for legals

---
+++ styles/legals.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LegalContainer = styled.div`
+  background-color: var(--main-color2);
+  margin-top: 2rem;
+  margin-bottom: calc(2rem + 3.4rem);
+  padding: 3rem;
+  width: 75vw;
+  max-width: 50rem;
+  border-radius: 2rem;
+`;
