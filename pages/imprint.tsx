6e51215967bf6a7dfbd49e8a78025c4676071654
kasi <danielkaser@mailbox.org>
2024-01-25 16:13:25 +0100

    add legal texts and images

--- pages/imprint.tsx
+++ pages/imprint.tsx
@@ -1,63 +1,111 @@
+import Image from "next/image";
 import { LoadingSpinner } from "../components";
 import { ILegalPageProps } from "../types/types";
-import { LegalContainer } from "../styles/legals.style";
+import { Country, LegalLink, LegalContainer } from "../styles/legals.style";
 
 export default function ImprintPage({ isLoading }: ILegalPageProps) {
   if (isLoading) return <LoadingSpinner />;
 
   return (
     <LegalContainer>
-      <h3>Imprint</h3>
-      <div>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-        est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in
-        hendrerit in vulputate velit esse molestie consequat, vel illum dolore
-        eu feugiat nulla facilisis at vero eros et accumsan et iusto odio
-        dignissim qui blandit praesent luptatum zzril delenit augue duis dolore
-        te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer
-        adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
-        dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-        nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
-        ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in
-        vulputate velit esse molestie consequat, vel illum dolore eu feugiat
-        nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
-        blandit praesent luptatum zzril delenit augue duis dolore te feugait
-        nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue
-        nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem
-        ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
-        nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut
-        wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
-        suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem
-        vel eum iriure dolor in hendrerit in vulputate velit esse molestie
-        consequat, vel illum dolore eu feugiat nulla facilisis. At vero eos et
-        accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-        sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
-        sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-        ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam
-        diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et
-        invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum.
-        sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
-        Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-        accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-        sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
-      </div>
+      <h2>Imprint</h2>
+      <h4>Impressum</h4>
+      <h3>Angaben gemäß § 5 TMG</h3>
+      <figure>
+        <Image
+          src="/images/address.png"
+          width={220}
+          height={80}
+          alt="Image that shows contact details"
+        />
+        <Country />
+      </figure>
+      <h3>Kontakt</h3>
+      <figure>
+        <Image
+          src="/images/telmail.png"
+          width={220}
+          height={57}
+          alt="Image that shows more contact details"
+        />
+        <Country />
+      </figure>
+      <h3>Umsatzsteuer-ID</h3>
+      <p>
+        Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:
+        <br />
+        DE 260 630 938
+      </p>
+      <h3>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h3>
+      <figure>
+        <Image
+          src="/images/address.png"
+          width={220}
+          height={80}
+          alt="Image that shows contact details again"
+        />
+        <Country />
+      </figure>
+      <p>
+        Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
+        vor einer Verbraucherschlichtungsstelle teilzunehmen.
+      </p>
+      <h4>Haftung für Inhalte</h4>
+      <p>
+        Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf
+        diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8
+        bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+        übermittelte oder gespeicherte fremde Informationen zu überwachen oder
+        nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
+        hinweisen.
+      </p>
+      <p>
+        Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+        Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
+        Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+        Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von
+        entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
+        entfernen.
+      </p>
+      <h4>Haftung für Links</h4>
+      <p>
+        Unser Angebot enthält Links zu externen Websites Dritter, auf deren
+        Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden
+        Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
+        Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten
+        verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+        Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte
+        waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+      </p>
+      <p>
+        Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
+        ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+        Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend
+        entfernen.
+      </p>
+      <h4>Urheberrecht</h4>
+      <p>
+        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+        Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
+        Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+        Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+        jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
+        sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+      </p>
+      <p>
+        Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden,
+        werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
+        Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+        Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+        entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden
+        wir derartige Inhalte umgehend entfernen.
+      </p>
+      <p>
+        <span>Quelle: </span>
+        <LegalLink href="https://www.e-recht24.de">
+          https://www.e-recht24.de
+        </LegalLink>
+      </p>
     </LegalContainer>
   );
 }
--- styles/legals.style.js
+++ styles/legals.style.js
@@ -1,11 +1,26 @@
 import styled from "styled-components";
+import Link from "next/link";
 
 export const LegalContainer = styled.div`
   background-color: var(--main-color2);
-  margin-top: 2rem;
-  margin-bottom: calc(2rem + 3.4rem);
+  margin-top: 1.5rem;
+  margin-bottom: calc(1.5rem + 3.4rem);
   padding: 3rem;
   width: 75vw;
   max-width: 50rem;
   border-radius: 2rem;
+`;
+
+export const LegalLink = styled(Link)`
+  color: var(--accent-color1);
+`;
+
+export const Country = styled.span`
+  display: block;
+  font-size: 0.85rem;
+  color: #666;
+
+  &::before {
+    content: "Germany";
+  }
 `;
